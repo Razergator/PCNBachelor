df0d0e36219d7cadf3d6417061d0d6baa20cf5a7
Joel Baltes <joelbaltes@web.de>
2024-05-20 13:05:36 +0200

    Some changes to the functions in PCNLayer, working on inference

--- PCN_2/Classification.py
+++ PCN_2/Classification.py
@@ -19,21 +19,24 @@ class PCNLayer(nn.Module):
         super(PCNLayer, self).__init__()
         self.prediction = nn.Linear(hidden_size, input_size)
         self.error = nn.Linear(input_size, hidden_size)
-        self.state = torch.random(hidden_size) #state is just the tensor that is saved in the layer
+        self.state = torch.rand(hidden_size) #state is just the tensor that is saved in the layer
         self.hidden_size = hidden_size
+    
+    
+    
+    def predict(self):
+        random_matrix = torch.ones((784, 784))
+        prediction = torch.matmul(self.state, random_matrix)
+        return prediction
 
+    def error_calculation(self, prediction):
+        error = self.state - prediction
+        return error
 
-        def predict(self):
-            prediction = torch.random() #placeholder
-            return prediction
-
-        def error_calculation(self, prediction):
-            error = self.state - prediction
-            return error
-
-        def recalculate_state(self,error):
-            state = self.state + error #placeholder
-            return state
+    def recalculate_state(self,error):
+        state = self.state + error #placeholder
+        print(state)
+        return state
 
 
     
@@ -47,9 +50,10 @@ class PCN(nn.Module):
         self.classifier.requires_grad_(False) # Prevent the layer from being updated during training
         self.relu = nn.ReLU() #ReLu layers which can be added
         self.bottom_layer = torch.zeros(hidden_size) #placeholder for now
+
+        
         
     def forward(self, input_data):
-        feedback = torch.zeros_like(input_data) # Initialize feedback signa
         cycles = 10  #Number is subject to change
         #feedback = self.relu(feedback) #add ReLu layer for non-linearity
 
@@ -57,10 +61,12 @@ class PCN(nn.Module):
         for _ in range(cycles): #first try at getting it cyclic, right now it just repeats multiple times
             predictions = []
             errors = []
+            
 
             #First all the predictions are calculated, then the errors and then the states are recalculated, so I don't have to access multiple layers at once
             for layer in self.layers:
-                predictions.append(layer.predict())
+                prediction = layer.predict()
+                predictions.append(prediction)
             for i in range(len(self.layers)):
                 errors.append(self.layers[i].error_calculation(predictions[i]))
             for i in range(len(self.layers)):
@@ -68,6 +74,7 @@ class PCN(nn.Module):
                     self.layers[i].state = self.layers[i].recalculate_state(errors[i])
             
         # Classification layer
+       
         bottom_layer = input_data #placeholder ish atm
         return bottom_layer
 
@@ -84,17 +91,18 @@ trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 # Define the PCN model
-input_size = 28 * 28  # MNIST image size
+input_size = 784  # MNIST image size
 hidden_size = 784
-num_layers = 2
+num_layers = 10
 pcn_model = PCN(input_size, hidden_size, num_layers)
+pcn_model.forward(trainloader)
 
 # Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(pcn_model.parameters(), lr=0.01)
 
 # Train the PCN model
-for epoch in range(5):  # Train for 5 epochs (you can increase it for better performance)
+'''for epoch in range(5):  # Train for 5 epochs (you can increase it for better performance)
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
@@ -115,13 +123,8 @@ for epoch in range(5):  # Train for 5 epochs (you can increase it for better per
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
-        losses.append(running_loss / len(trainloader))
+        losses.append(running_loss / len(trainloader))'''
 
 # Visualize the training loss
-plt.figure()
-plt.plot(losses)
-plt.title('Training Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.show()
+
 print('Finished Training')

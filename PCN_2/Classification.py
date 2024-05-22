725e42acd8bcbe77238d3d7f303caeee06eb7e47
Joel Baltes <joelbaltes@web.de>
2024-05-22 12:39:47 +0200

    Changes to the trainloader and the trainloop

--- PCN_2/Classification.py
+++ PCN_2/Classification.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-import torch.nn.functional as F
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
 
@@ -17,26 +17,27 @@ losses = [] #list for visualizing loss
 class PCNLayer(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(PCNLayer, self).__init__()
-        self.prediction = nn.Linear(hidden_size, input_size)
-        self.error = nn.Linear(input_size, hidden_size)
-        self.state = torch.rand(hidden_size) #state is just the tensor that is saved in the layer
+        self.state = torch.rand(hidden_size,requires_grad=True) #state is just the tensor that is saved in the layer
         self.hidden_size = hidden_size
     
     
     
     def predict(self):
-        random_matrix = torch.ones((784, 784))
-        prediction = torch.matmul(self.state, random_matrix)
+        W_matrix = torch.ones((10, 10), requires_grad=True) #placeholder for now
+        prediction = torch.matmul(self.state, W_matrix)
+        
         return prediction
 
     def error_calculation(self, prediction):
         error = self.state - prediction
+        
         return error
 
     def recalculate_state(self,error):
-        state = self.state + error #placeholder
-        print(state)
-        return state
+        self.state = self.state
+
+        
+        return self.state
 
 
     
@@ -47,10 +48,12 @@ class PCN(nn.Module):
         super(PCN, self).__init__()
         self.layers = nn.ModuleList([PCNLayer(input_size, hidden_size) for _ in range(num_layers)])
         self.classifier = nn.Linear(hidden_size, num_classes)
-        self.classifier.requires_grad_(False) # Prevent the layer from being updated during training
+        #self.classifier.requires_grad_(False) # Prevent the layer from being updated during training
         self.relu = nn.ReLU() #ReLu layers which can be added
         self.bottom_layer = torch.zeros(hidden_size) #placeholder for now
 
+        #self.layers[num_layers-1].state = torch.tensor([0,1,2,3,4,5,6,7,8,9]).float().requires_grad_(True) #placeholder for now
+        
         
         
     def forward(self, input_data):
@@ -62,7 +65,6 @@ class PCN(nn.Module):
             predictions = []
             errors = []
             
-
             #First all the predictions are calculated, then the errors and then the states are recalculated, so I don't have to access multiple layers at once
             for layer in self.layers:
                 prediction = layer.predict()
@@ -70,13 +72,21 @@ class PCN(nn.Module):
             for i in range(len(self.layers)):
                 errors.append(self.layers[i].error_calculation(predictions[i]))
             for i in range(len(self.layers)):
-                if i > 0: #First layer is fixed
+                if i > 0 and i < len(self.layers)-1:  #First layer and last layers are fixed
                     self.layers[i].state = self.layers[i].recalculate_state(errors[i])
+        
+        out = self.layers[num_layers-1].state
+
+        return out
+            
+                   
+
+           
             
         # Classification layer
        
-        bottom_layer = input_data #placeholder ish atm
-        return bottom_layer
+       
+        
 
    
 
@@ -88,11 +98,11 @@ transform = transforms.Compose([
 
 # Load the MNIST dataset
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=10, shuffle=True) #is normally 64, i dont really get it to work now
 
 # Define the PCN model
-input_size = 784  # MNIST image size
-hidden_size = 784
+input_size = 24*24  # MNIST image size
+hidden_size = 10
 num_layers = 10
 pcn_model = PCN(input_size, hidden_size, num_layers)
 pcn_model.forward(trainloader)
@@ -102,11 +112,12 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(pcn_model.parameters(), lr=0.01)
 
 # Train the PCN model
-'''for epoch in range(5):  # Train for 5 epochs (you can increase it for better performance)
+for epoch in range(5):  # Train for 5 epochs (you can increase it for better performance)
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
         inputs = inputs.view(-1, 28 * 28)  # Flatten the input images
+        labels = labels.float()
         
         optimizer.zero_grad()
         
@@ -123,7 +134,7 @@ optimizer = optim.SGD(pcn_model.parameters(), lr=0.01)
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
-        losses.append(running_loss / len(trainloader))'''
+        losses.append(running_loss / len(trainloader))
 
 # Visualize the training loss
 

13d6a3acf2dde0f16912db611c7eba47754cffc5
Joel Baltes <joelbaltes@web.de>
2024-06-16 21:16:15 +0200

    Finishin touches (added optimization, cleaned up code)

--- PCN_2/Classification.py
+++ PCN_2/Classification.py
@@ -5,59 +5,73 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 losses = [] #list for visualizing loss
 
-#defining all necessary methods here, maybe I need to put them into PCNLayer class, unsure yet
+W = torch.empty(64, 64) #duplikat bei recalculate state einfügen mit richtigen dimensionen
+torch.nn.init.xavier_uniform_(W)
 
+def relu_prime(x): #ReLu derivative is 1 if x > 0, 0 otherwise
+    return (x > 0).float()
 
+#own optimizer will be defined here
+def matrix_update(learning_rate, error, state_1):#
+    #return learning_rate * (error * relu_prime(state_1).T)
+    #pass
+    #print("error shape:", error.shape)
+    #print("relu_prime(state_1).T shape:", relu_prime(state_1).T.shape)
+    new_W = learning_rate * (error * relu_prime(state_1).T)
+    print("test",new_W.shape)
+    W = new_W 
+    return new_W
+    
 
 # Define the PCNLayer class, maybe I only need to have self.state as a tensor and not the other variables
 class PCNLayer(nn.Module):
-    def __init__(self, input_size, output_size, batch_size = 64):
+    def __init__(self, input_size, output_size):
         super(PCNLayer, self).__init__()
         self.output = output_size
-        #self.state = torch.rand(batch_size, output_size)#state is just the tensor that is saved in the layer
-        #self.state = torch.rand(batch_size, hidden_size)
         self.linear = nn.Linear(input_size, output_size, bias=False)#torch.ones((10, 10), requires_grad=True)
         self.relu = nn.ReLU()
-        #self.hidden_size = hidden_size
     
     
     
     def predict(self):
         relu_state = self.relu(self.state)
-        #print("State:", self.state.shape)
-        #print("self.linear:", relu_state.shape)
         prediction = self.linear(relu_state)
-        #print("Prediction:", prediction.shape)
         return prediction
 
     def error_calculation(self, prediction):
-        print("prediction:", prediction.shape)
-        print("State:", self.state.shape)
+        #print("prediction:", prediction.shape)
+        #print("State:", self.state.shape)
         error = self.state - prediction
         
         return error
 
-    def recalculate_state(self,error):
-        print("State",self.state.shape)
-        print("Error", error.shape)
-        self.state = self.state - error #brain inspired computation via predictive coding
-        #print(self.state)
-
-        #return self.state
+    def recalculate_state(self, learning_rate, e_0,e_1, W): #e_i = errsor, W = weight_matrix, phi_prime = derivative of an activation function(here ReLu)
+        phi_prime = relu_prime(self.state)
+        #print("phi_prime",phi_prime.shape)
+        #print("W.T@e_1",(W.T @ e_1).shape)
+        
+        self.state += learning_rate * (-e_0 + (phi_prime))  * (W.T @ e_1)
+        print(self.state) 
+       
 
 
     
     
 # Define the PCN class
 class PCN(nn.Module):
-    def __init__(self, hidden_sizes):
+    def __init__(self, hidden_sizes, W):
         super(PCN, self).__init__()
         self.hidden_sizes = hidden_sizes
         self.layers = nn.ModuleList()
+        self.W = W
+        
+        
+        #print("W",W)
         for i in range(len(hidden_sizes)):
             if i == 0:
                 print("i=0",hidden_sizes[i])
@@ -67,18 +81,11 @@ class PCN(nn.Module):
                 print("i>0",hidden_sizes[i],hidden_sizes[i-1])
                 self.layers.append(PCNLayer(hidden_sizes[i],hidden_sizes[i-1]))
                 print("layer:",self.layers[i])
+        
 
-        
-        #self.classifier = nn.Linear(hidden_size, num_classes)
-    
-         
-        
-        
-        
     def forward(self, input_data):
         cycles = 10  #Number is subject to change
-        #feedback = self.relu(feedback) #add ReLu layer for non-linearity
-        #self.layers[0].state = input_data, dk how to fix rn
+        
         # Forward pass through PCN layers
         for i in range(len(self.layers)):
             #if i > 0:
@@ -88,7 +95,6 @@ class PCN(nn.Module):
         for _ in range(cycles): 
             predictions = []
             errors = []
-
             
             #First all the predictions are calculated, then the errors and then the states are recalculated, so I don't have to access multiple layers at once
             for layer in self.layers[1:]: #first layer no prediciton
@@ -96,19 +102,23 @@ class PCN(nn.Module):
                 predictions.append(prediction)
             for i in range(len(self.layers)-1): #last layer no error
                 errors.append(self.layers[i].error_calculation(predictions[i]))
+            
             for i in range(len(self.layers)):
-                if i > 0 and i < 3:  #First layer is fixed
-                    self.layers[i].recalculate_state(errors[i])
-        
-        out = self.layers[len(self.layers)-1].state  #output of the last layer 
+                if i > 0 and i < len(self.layers)-2:  #First layer is fixed
+                    self.layers[i].recalculate_state(0.01,errors[i],errors[i+1],self.W)
+                elif i == len(self.layers)-2:
+                    self.layers[i].recalculate_state(0.01,errors[i],errors[i],self.W)
+                
+            
+            for i in range(len(self.layers)):
+                if i == len(self.layers)-2:
+                    self.W = matrix_update(0.01, errors[i], self.layers[i+1].state)
+                    print("W",self.W)
 
+        out = self.layers[len(self.layers)-1].state  #output of the last layer
         return out
             
-       
-       
-        
-
-   
+    
 
 # Define the MNIST image transformation
 transform = transforms.Compose([
@@ -118,29 +128,25 @@ transform = transforms.Compose([
 
 # Load the MNIST dataset
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True) #is normally 64, i dont really get it to work now
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True) 
 
 # Define the PCN model
 input_size = 28*28  # MNIST image size
 hidden_size = 10
 num_layers = 10
-pcn_model = PCN([700,100,100,100])
-for i, data in enumerate(trainloader):
-    # data is a tuple of (inputs, labels)
-    inputs, labels = data
-    pcn_model.forward(inputs)
-    # Now you can use inputs and labels
-    print(inputs)
-    print(labels)
+#pcn_model = PCN([700,225,100,64])
+pcn_model = PCN([64,64,64,64],W)
 
-    # If you only want the first batch, you can break after the first iteration
-    if i == 0:
-        break
- #just to test if it works, add input data instead of trainloader
 
-# Define the loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(pcn_model.parameters(), lr=0.01)
+for epoch in range(5):
+    for i, data in enumerate(trainloader):
+        # data is a tuple of (inputs, labels)
+        inputs, labels = data
+        pcn_model.forward(inputs)
+        
+
+        # If you only want the first batch, you can break after the first iteration
+       
 
 # Train the PCN model
 '''for epoch in range(5):  # Train for 5 epochs (you can increase it for better performance)
@@ -163,7 +169,7 @@ criterion = nn.CrossEntropyLoss()
         
         # Backward pass and optimization
         loss.backward()
-       #optimizer.step()
+        #optimizer.step()
         
         running_loss += loss.item()
         if i % 100 == 99:  # Print every 100 mini-batches
